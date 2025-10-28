e6e6a382728a77886ed31f5b950fa01d31716968
vietpv2005 <pvviet2005@gmail.com>
2025-10-28 10:52:07 +0700

    login-register

--- backend/src/models/registerAccModel.ts
+++ backend/src/models/registerAccModel.ts
@@ -8,15 +8,16 @@ export const Check_email = async (email: string): Promise<{ email: string }[]> =
 };
 
 export const Register = async (
+  username: string,
   email: string,
   password: string,
 ): Promise<number> => {
   const sql = `
-    INSERT INTO account (email, mat_khau_hash )
-    VALUES ($1, $2)
+    INSERT INTO account (ho_ten,email, mat_khau_hash)
+    VALUES ($1, $2 , $3)
     RETURNING id_account
   `;
-  const result = await pool.query(sql, [email, password]);
+  const result = await pool.query(sql, [username,email, password]);
   return result.rows[0].id_account as number;
 };
 
