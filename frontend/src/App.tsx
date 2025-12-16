f34533515947c4868d675866abe16ad7ef8f000f
Pnam1408 <nam19881408@gmail.com>
2025-12-16 22:22:49 +0700

    dọn đẹp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import  Dashboard  from "./components/Dashboard";
-import Component from "./components/Component";
+import  DashBoard  from "./components/DashBoard";
 import Login from "./components/Login";
 //import ForgetPassword from "./components/test"
 //import UpdateUserForm from "./components/test"
@@ -8,8 +7,8 @@ import BorrowBooks from "./components/BorrowBooks"
 import Register from "./components/Register"
 //import HistoryButton from "./components/test";
 //import ListAccountPage from "./components/test";
-import AddBook from "./components/test";
-import AddBanSao from "./components/test";
+//import AddBook from "./components/test";
+//import AddBanSao from "./components/test";
 //import LateListPage from "./components/test";
 import PersonalInfoInsert from "./components/PersonInfoInsert"
 import LibraryCard from "./components/LibraryCard";
@@ -21,40 +20,29 @@ import BorrowHistoryUser from "./components/BorrowHistoryUser";
 import UpdateReturnDate from "./components/UpdateReturnDate";
 import PasswordChange from "./components/PasswordChange";
 import ExtendCard from "./components/ExtendCard";
-import BorrowHistory from "./components/BorrowHistory";
 //import type UpdateReturnDate from "./Components/test";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Component />} />
+        <Route path="/" element={<DashBoard />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/personal-info-insert" element={<PersonalInfoInsert />} />
         <Route path="/update" element={<UpdateReturnDate />} />
         <Route path="/accinfo" element={<AccInfo />} />
         <Route path="/password-change" element={<PasswordChange />} />
         <Route path="/librarycard" element={<LibraryCard />} />
         <Route path="/extendcard" element={<ExtendCard />} />
-        <Route path="/borrowhistory" element={<BorrowHistory />} />
-        <Route path="/borrowhistoryuser" element={<BorrowHistoryUser />} />
         <Route path="/borrowbooks" element={<BorrowBooks />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/add" element={<AddBook />} />
-        <Route path="/add-ban-sao" element={<AddBanSao />} />
-         <Route path="/lock" element={<LockAccount />} />
+        <Route path="/lock" element={<LockAccount />} />
         <Route path="/patronlist" element={<PatronList />} />
         <Route path="/history-user" element={<BorrowHistoryUser />} />
         <Route path="/librarycard" element={<LibraryCard />} />
         <Route path="/addbook" element={<AddBookForm />} />
-        <Route path="/lock" element={<LockAccount />} />
-        <Route path="/patronlist" element={<PatronList />} />
       </Routes>
     </BrowserRouter>
   );
